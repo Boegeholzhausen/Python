fedf6f75dae7fcc994ac182ffdea4891c0a0c5ba
Boegeholzhausen <jannisboeg@gmail.com>
2021-04-27 00:19:05 +0200

    website with flask tut

--- WPMTracker/graph.py
+++ WPMTracker/graph.py
@@ -24,17 +24,42 @@ for i in range(start2, 1000):
         wpmvalues.append(sheet.cell(row=i, column=3).value)
         start += 1
 
+#accvalues = []
+#start2 = 2
+#for i in range(start2, 1000):
+#    if sheet.cell(row=i, column=4).value != None:
+#        accvalues.append(sheet.cell(row=i, column=4).value)
+#        start += 1
+#print(accvalues)
+
+
 # Data
-df=pd.DataFrame({'xvalues': nrvalues, 'yvalues': wpmvalues})
+dfwpm=pd.DataFrame({'xvalues': nrvalues, 'yvalues': wpmvalues})
+#dfacc=pd.DataFrame({'xaccvalues': nrvalues, 'yaccvalues': accvalues})
 
 # Plot
 plt.style.use("seaborn")
-plt.figure(figsize=(24,8))
+plt.figure(figsize=(22,6))
 plt.rcParams["figure.figsize"]
-plt.plot('xvalues', 'yvalues', color="#2F78BB", linewidth=3, label='WPM Tracker', data=df)
+plt.plot('xvalues', 'yvalues', color="#2F78BB", linewidth=3, label='WPM Tracker (WPM)', data=dfwpm)
 plt.title("WPM Tracker")
 plt.xlabel("Nr.")
 plt.ylabel("WPM")
 plt.grid(True)
 plt.tight_layout()
+
+
+#plt.style.use("seaborn")
+#plt.figure(figsize=(20,5))
+#plt.rcParams["figure.figsize"]
+#plt.plot('xaccvalues', 'yaccvalues', color="#2F78BB", linewidth=3, label='WPM Tracker (Accuracy)', data=dfacc)
+#plt.title("WPM Tracker")
+#plt.xlabel("Nr.")
+#plt.ylabel("WPM")
+#plt.grid(True)
+#plt.tight_layout()
+#
+
+
 plt.show()
+
