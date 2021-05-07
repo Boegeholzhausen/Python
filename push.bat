8b21d468d5e59c81a898b75c850a42bf0e212c14
Boegeholzhausen <jannisboeg@gmail.com>
2021-05-07 20:16:40 +0200

    new records

---
+++ push.bat
@@ -0,0 +1,1 @@
+@py.exe C:\Python\push.py %*
---
+++ push.py
@@ -0,0 +1,16 @@
+import pyautogui, time
+
+
+pyautogui.typewrite(["winleft", "g", "i", "t", "enter"], interval=0.1)
+time.sleep(1)
+pyautogui.typewrite(["c", "d", " ", "C", ":", "enter"], interval=0.1)
+time.sleep(0.2)
+pyautogui.typewrite(["c", "d", " ", "P", "y", "t", "h", "o", "n", "enter"], interval=0.1)
+time.sleep(0.2)
+pyautogui.typewrite(["g", "i", "t", " ", "s", "t", "a", "t", "u", "s", "enter"], interval=0.1)
+time.sleep(0.2)
+pyautogui.typewrite(["g", "i", "t", " ", "a", "d", "d", " ", ".", "enter"], interval=0.1)
+time.sleep(0.2)
+pyautogui.typewrite(["g", "i", "t", " ", "c", "o", "m", "m", "i", "t", " ", "-", "m", " ", "\"", "n", "e", "w", " ", "r", "e", "c", "o", "r", "d", "s", "\"", "enter"], interval=0.1)
+time.sleep(0.2)
+pyautogui.typewrite(["g", "i", "t", " ", "p", "u", "s", "h", " ", "o", "r", "i", "g", "i", "n", " ", "m", "a", "s", "t", "e", "r","enter"], interval=0.1)
